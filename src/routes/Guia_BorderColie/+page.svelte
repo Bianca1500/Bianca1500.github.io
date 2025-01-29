ae16be4e0f72dce11539ddc9e5f63513536203f1
Bianca1500 <biancatavarescordeiro33@gmail.com>
2025-01-29 17:20:13 -0400

    nav bar

---
+++ src/routes/Guia_BorderColie/+page.svelte
@@ -0,0 +1,53 @@
+<body>
+	<nav calss="navbar">
+		<ul>
+			<li>
+				<a href="/Origem">Origem</a>
+			</li>
+			<li>
+				<a href="/Comportamento">Comportamento</a>
+			</li>
+			<li>
+				<a href="#">Saúde</a>
+			</li>
+			<li>
+				<a href="#">Aparência física</a>
+			</li>
+			<li>
+				<a href="#">Preço</a>
+			</li>
+			<li>
+				<a href="#">Raças semelhantes</a>
+			</li>
+		</ul>
+	</nav>
+
+</body>
+
+<style>
+
+	.navbar{
+		background-color: #37add1;
+	}
+
+body{
+	background-color:white;
+	font-family:Arial, Helvetica, sans-serif;
+	margin-right: 20px;
+}
+
+	ul{
+		display:flex;
+		justify-content: space-between;
+		list-style-type: none;
+		align-items: center;
+		padding: 16px;
+
+	}
+
+	a{
+		color:black;
+		text-decoration: none;
+	}
+
+</style>
